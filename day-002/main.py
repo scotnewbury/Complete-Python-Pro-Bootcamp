61948294ecfd81b1138e1aecebd29849b0d9b2fe
Scot Newbury <scot@scotnewbury.com>
2023-08-06 17:50:59 -0400

    Completed through lesson 19 and quiz 2

--- day-002/main.py
+++ day-002/main.py
@@ -11,3 +11,13 @@ print(123+345)
 # Can use underscore instead of a comman
 
 123_456_789  # equivalent to 123,456,789
+
+# Float or floating point number
+# Has a decimal point
+
+3.14159
+
+# Boolean
+
+True
+False
