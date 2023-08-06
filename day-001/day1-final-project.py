e151009eca17e9587546b3ff08fb25c104391cc4
Scot Newbury <scot@scotnewbury.com>
2023-08-06 01:23:16 -0400

    Completed Day 1 including the day 1 final project

---
+++ day-001/day1-final-project.py
@@ -0,0 +1,9 @@
+# Create a program that asks the user for the town they grew up in
+# and the name of their pet and use the two to output a band name
+
+print ("Welcome to the Band Name Generator")
+
+town_name = input ("What was the name of the town where you grew up?\n")
+pet_name = input ("What is the name of your pet?\n")
+
+print ("Your new band name is: " + town_name + " " + pet_name)
