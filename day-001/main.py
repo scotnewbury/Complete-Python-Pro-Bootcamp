5135fe37bf5f025f19c6ce673db6d13dcb54f931
Scot Newbury <scot@scotnewbury.com>
2023-08-06 17:34:29 -0400

    Clear up linting

--- day-001/main.py
+++ day-001/main.py
@@ -1,6 +1,6 @@
-name = input ("What is your name? ")
-print (name)
+name = input("What is your name? ")
+print(name)
 
 name = input("What is your nme? ")
-length = len (name)
-print (length)
+length = len(name)
+print(length)
