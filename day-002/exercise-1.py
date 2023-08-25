1ec79476023438750aabaaf968674c7694646d14
Scot Newbury <scot@scotnewbury.com>
2023-08-25 16:19:10 -0400

    Cleaning up styling errors

--- day-002/exercise-1.py
+++ day-002/exercise-1.py
@@ -3,7 +3,7 @@ two_digit_number = input("Type a two digit number: ")
 # 🚨 Don't change the code above 👆
 
 ####################################
-#Write your code below this line 👇
+# Write your code below this line 👇
 
 ones_digit = int(two_digit_number[0])
 tens_digit = int(two_digit_number[1])
