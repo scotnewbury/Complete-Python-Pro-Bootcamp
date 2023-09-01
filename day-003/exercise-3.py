1e58dc05b156681da260f5607db8281695995523
Scot Newbury <scot@scotnewbury.com>
2023-09-01 01:41:07 -0400

    Corrected divisible by 4 calculation and removed testing statements

--- day-003/exercise-3.py
+++ day-003/exercise-3.py
@@ -4,21 +4,15 @@ year = int(input("Which year do you want to check? "))
 
 # Write your code below this line 👇
 
-print(f"The year is: {year}")
-
-divisible_by_4 = year % 2
+divisible_by_4 = year % 4
 divisible_by_100 = year % 100
 divisible_by_400 = year % 400
 
-print(f"Divisible by   4 is {divisible_by_4}")
-print(f"Divisible by 100 is {divisible_by_100}")
-print(f"Divisible by 400 is {divisible_by_400}")
-
 if divisible_by_4:
-    print("Not leap year - not divisible by 4.")
+    print("Not leap year.")
 elif not divisible_by_100:
     if not divisible_by_400:
-        print("Not leap year - divisible by 400.")
-    print("Not leap year - divisible by 100.")
+        print("Not leap year.")
+    print("Not leap year.")
 else:
     print("Leap year.")
